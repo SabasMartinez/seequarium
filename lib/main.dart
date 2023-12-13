2a14f1994b21f0be8901ae78aed82a501852a0c6
Sabas Martinez <martinez.sabas3443@gmail.com>
2023-12-12 23:28:07 -0800

    added source

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'firebase_options.dart';
+import 'theme.dart';
 import 'router.dart';
 
-void main() {
-  runApp(const MyApp());
+//main entry point
+//initializes firebase, sharedpreferences, and removes splash screen
+//routes to welcome screen if first run, home screen if not
+// Routes are in router.dart, screens and widgets are in lib/screens
+void main() async {
+  WidgetsBinding binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //persistent across routes.
+  await SharedPreferencesSingleton.createInstance();
+  runApp(const Seequarium());
+  //checks if it is first run, reroutes to home if not
+  entrypoint();
+  FlutterNativeSplash.remove();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Seequarium extends StatelessWidget {
+  //construct a seequarium object
+  const Seequarium({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      theme: appTheme,
+      routerConfig: router,
+      title: "Seequarium",
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+void entrypoint() {
+  SharedPreferences preferences = SharedPreferencesSingleton.preferences!;
+  if (isFirstRun(preferences)) {
+    //if first run, route to welcome screen
+    router.go("/welcome");
+  } else {
+    //if not first run, route to home screen
+    router.go("/home");
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+bool isFirstRun(SharedPreferences? preferences) {
+  if (preferences != null) {
+    if (preferences.getBool("welcomeDismissed") == null ||
+        preferences.getBool("welcomeDismissed") == false) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  throw Exception("SharedPreferences is null");
+}
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+//create singleton sharedpreferences
+class SharedPreferencesSingleton {
+  static SharedPreferencesSingleton? _instance;
+  static SharedPreferences? _preferences;
+
+  static Future<SharedPreferencesSingleton?> createInstance() async {
+    _instance ??= SharedPreferencesSingleton();
+    _preferences ??= await SharedPreferences.getInstance();
+    return _instance;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  static SharedPreferences? get preferences => _preferences;
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,24 +1,169 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
-import 'screens/welcome.dart';
+import 'screens/identify_screen.dart';
+import 'screens/locate_screen.dart';
+import 'screens/welcome_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/profile_screen.dart';
 import 'screens/settings_screen.dart';
+import 'screens/maintainance_screen.dart';
 
-final router = GoRouter(
+//global key to show child routes.
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+final GoRouter router = GoRouter(
+  navigatorKey: navigatorKey,
+  // redirect:  doRedirect(),
+  // initialLocation: '/welcome',
   routes: [
-    GoRoute(
-      path: '/',
-      pageBuilder: (context, state) => HomeScreen(),
-    ),
-    GoRoute(
-      path: '/profile/:id',
-      pageBuilder: (context, state) => ProfileScreen(id: state.params['id']),
-    ),
-    GoRoute(
-      path: '/settings',
-      pageBuilder: (context, state) => SettingsScreen(),
-    ),
+    ShellRoute(
+      builder: (context, state, child) => Scaffold(
+        appBar: (matchPath(state.matchedLocation) == "Welcome")
+            ? null
+            : AppBar(
+                title: Text(matchPath(state.matchedLocation)),
+                leading: Builder(
+                  builder: (context) => IconButton(
+                    icon: const Icon(Icons.menu),
+                    onPressed: () => Scaffold.of(context).openDrawer(),
+                  ),
+                ),
+              ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage('assets/images/aquarium1.jpg'),
+                  fit: BoxFit.cover,
+                )),
+                child: Text(
+                  'Seequarium',
+                  style: TextStyle(
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10.0,
+                        color: Colors.black,
+                        offset: Offset(5.0, 5.0),
+                      ),
+                    ],
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              //welcome is not a tab that is accessible by the user,
+              // this is shown once at first start so it should not be in the drawer
+              // ListTile(
+              //   title: const Text('Welcome'),
+              //   onTap: () {
+              //     context.go('/welcome');
+              //     Navigator.pop(context); // Close the drawer
+              //   },
+              // ),
+              ListTile(
+                title: const Text('Home'),
+                onTap: () {
+                  context.go('/home');
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+              ListTile(
+                title: const Text('Profile'),
+                onTap: () {
+                  context.go('/profile');
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+              ListTile(
+                title: const Text('Log Maintainance'),
+                onTap: () {
+                  context.go('/maintainance');
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+              ListTile(
+                title: const Text('Locate'),
+                onTap: () {
+                  context.go('/locate');
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+              ListTile(
+                title: const Text('Identify'),
+                onTap: () {
+                  context.go('/identify');
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+              ListTile(
+                title: const Text('Settings'),
+                onTap: () {
+                  context.go('/settings');
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+            ],
+          ),
+        ),
+        body: child,
+      ),
+      routes: [
+        GoRoute(
+          name: "Home",
+          path: '/home',
+          builder: (context, state) => const HomeScreen(),
+        ),
+        GoRoute(
+          name: "Welcome",
+          path: '/welcome',
+          builder: (context, state) => const WelcomeScreen(),
+        ),
+        GoRoute(
+          path: '/profile',
+          builder: (context, state) => const ProfileScreen(),
+        ),
+        GoRoute(
+          path: '/settings',
+          builder: (context, state) => const SettingsScreen(),
+        ),
+        GoRoute(
+          path: '/locate',
+          builder: (context, state) => const LocateScreen(),
+        ),
+        GoRoute(
+          path: '/identify',
+          builder: (context, state) => const IdentifyScreen(),
+        ),
+        GoRoute(
+          path: '/maintainance',
+          builder: (context, state) => const MaintainanceScreen(),
+        ),
+      ],
+    )
   ],
 );
+
+String matchPath(String path) {
+  switch (path) {
+    case '/welcome':
+      return 'Welcome';
+    case '/home':
+      return 'Home';
+    case '/profile':
+      return 'Profile';
+    case '/settings':
+      return 'Settings';
+    case '/locate':
+      return 'Locate';
+    case '/identify':
+      return 'Identify';
+    case '/maintainance':
+      return 'Log Maintainance';
+    default:
+      return 'Welcome';
+  }
+}
